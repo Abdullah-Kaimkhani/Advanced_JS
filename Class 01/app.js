f6d24eaede7fa207c93dd407e03ecc59d5e793af
abdullah11137 <javediqbal123999@gmail.com>
2024-11-16 21:24:58 +0500

    Add files via upload

---
+++ Class 01/app.js
@@ -0,0 +1,61 @@
+// Difference b/w var, let & const
+
+
+// {
+//     var firstName = "Abdullah";
+//     console.log(firstName);
+    
+// }
+
+// var firstName = "Kaimkhani";
+// console.log(firstName);
+
+
+
+// let firstName = "Kaimkhani";
+
+// {
+//     const firstName = "Abdullah";
+//     console.log(firstName);
+    
+// }
+// console.log(firstName);
+
+
+// function num(){
+//     var nums = 10;
+//     return nums;
+// }
+// console.log(nums);
+// num()
+
+// var num1 = num();
+// console.log(num1);
+
+
+
+// var firstName = "Abdullah";
+// var firstName = "Kaimkhani";
+
+// console.log(firstName);
+// console.log(firstName);
+
+
+// In JS, hoisted can be done through two things.
+// 1- var
+// 2- function
+
+// console.log(num);
+
+// let num = 10;
+
+// greet();
+// console.log(greet);
+
+
+// let greet = function(){
+//     console.log("Welcome user!!")
+// }
+
+
+
