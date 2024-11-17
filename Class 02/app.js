13c24c4719b5466f74062664eaab2baaf8349ae4
abdullah11137 <javediqbal123999@gmail.com>
2024-11-17 21:08:06 +0500

    Add files via upload

---
+++ Class 02/app.js
@@ -0,0 +1,60 @@
+// console.log(name);
+// var name = "Abdullah Kaimkhani"
+
+
+// let num = +prompt("Enter any number");
+// let range = +prompt("Enter range");
+
+// for(let i=1; i<=range; i++){
+//     document.write(`${num} x ${i} = ${num * i} <br>`)
+// }
+
+
+// let age = +prompt("Enter your age");
+// let check = age >= 18 ? 'Allow' : 'Not allow';
+// console.log(check);
+
+
+// let age = 24;
+// let gender = "male";
+
+// let check = age ===25 ? gender ==="male"? 'allow' : 'gender not match' : 'age not match';
+// console.log(check);
+
+
+
+// let percentage = +prompt("Enter your percentage")
+// let check = percentage < 50 ? 'You are failed' : percentage >=50 && percentage<60? 'Your grade is C' : percentage >= 60 && percentage < 70? 'Your grade is B' : percentage >=70 && percentage<80? 'Your grade is A' : percentage >=80 && percentage<= 100? 'Your grade is A+' : 'Please enter valid percentage';
+// console.log(check);
+
+// if(percentage < 50){
+//     alert("You are failed");
+// }else if(percentage >=50 && percentage<60){
+//     alert("YOUR grade is C")
+// }else if(percentage >= 60 && percentage < 70){
+//     alert("YOUR grade is B")
+// }else if(percentage >=70 && percentage<80){
+//     alert("YOUR grade is A")
+// }else if(percentage >=80 && percentage<= 100){
+//     alert("YOUR grade is A+")
+// }else{
+//     alert("Please enter valid percentage")
+// }
+
+
+
+let age = +prompt("Enter age");
+
+let check = age>=18? 'let gender = prompt("Enter gender")' : gender ==="male" ? 'Allow' : 'Gender not match' : 'Age not match'
+
+
+// if(age >=18){
+//     let gender = prompt("Enter your gender")
+//     if(gender ==="male"){
+//         alert("Allow")
+//     }else{
+//         alert("GEnder not match")
+//     }
+// }else{
+//     alert("Age not match")
+// }
